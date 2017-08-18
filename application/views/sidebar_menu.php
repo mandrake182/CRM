278e077d18da9b7f086cf96d6540e5e7c9674ad6
pablo maya <mayajahir8@gmail.com>
2017-08-18 17:10:47 -0500

    menu updates2

--- application/views/sidebar_menu.php
+++ application/views/sidebar_menu.php
@@ -1,29 +1,29 @@
 <div class="app-dashboard-sidebar-inner bgGrey">
   <ul class="menu vertical">
   	<li>
-       <a href="<?php echo base_url(); ?>" class="is-active">
+       <a href="<?php echo base_url(); ?>" class="<?php echo (isset($inicio) && $inicio==1)?'is-active':''; ?>">
           <i class="fi-home colorBlueDark"></i><span class="app-dashboard-sidebar-text">Inicio</span>
         </a>
     </li>
     <li>
-       <a href="<?php echo base_url().'Login/editar/'.$this->session->userdata('id_usuario'); ?>" class="">
+       <a href="<?php echo base_url().'Login/editar/'.$this->session->userdata('id_usuario'); ?>" class="<?php echo (strpos($_SERVER['REQUEST_URI'], 'Login/editar') || strpos($_SERVER['REQUEST_URI'], 'Login/update_user')) ? 'is-active':''; ?>">
           <i class="fi-page-edit colorBlueDark"></i><span class="app-dashboard-sidebar-text">Editar perfil</span>
         </a>
     </li>
 
     <li>
-    	   <a href="<?php echo base_url().'Login/password'; ?>">
+    	   <a href="<?php echo base_url().'Login/password'; ?>" class="<?php echo (strpos($_SERVER['REQUEST_URI'], 'Login/password') || strpos($_SERVER['REQUEST_URI'], 'Login/update_password')) ? 'is-active':''; ?>">
            <i class="fi-unlock colorBlueDark"></i><span class="app-dashboard-sidebar-text">Modificar password</span>
          </a>
      </li>
 
     <li>
-    	  <a href="<?php echo base_url().'Requision'; ?>"> 
+    	  <a href="<?php echo base_url().'Requisicion'; ?>" class="<?php echo (strpos($_SERVER['REQUEST_URI'], 'Requisicion') || strpos($_SERVER['REQUEST_URI'], 'Requisicion/Requisicion/detalles')) ? 'is-active':''; ?>"> 
       	<i class="fi-page colorBlueDark"></i><span class="app-dashboard-sidebar-text">Mis requisiciones</span>
         </a>
     </li>
      <li>
-    	   <a href="<?php echo base_url().'Requision/nueva_requisicion'; ?>">
+    	   <a href="<?php echo base_url().'Requisicion/nueva_requisicion'; ?>" class="<?php echo (strpos($_SERVER['REQUEST_URI'], 'Requisicion/nueva_requisicion')) ? 'is-active':''; ?>">
       	<i class="fi-page-add colorBlueDark"></i><span class="app-dashboard-sidebar-text">Nueva requisición</span>
         </a>
     </li>
