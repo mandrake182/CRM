93242108864686102a9119d82dec1448bb8dfffc
Gersain <gersigner@hotmail.com>
2017-08-18 16:14:55 -0500

    style requisiscionc v- 1

--- application/views/editar_view.php
+++ application/views/editar_view.php
@@ -10,32 +10,28 @@ require('header.php');
 	      	<img src="<?php echo base_url();?>img/logo.png" alt="">
 	      </a>
 	    </div>
-
-	    <!-- <div class="">
-	      	<button href="#" class="button hollow">CERRAR SESIÓN</button>
-	      	<a href="#" height="30" width="30" alt=""><i class="fa fa-info-circle"></i></a>
-	    </div> -->
       </div>
   </div>
 
 
-  <div class="app-dashboard-body off-canvas-wrapper">
-    <div id="app-dashboard-sidebar" class="app-dashboard-sidebar position-left off-canvas off-canvas-absolute reveal-for-medium" data-off-canvas>
-      <div class="app-dashboard-sidebar-title-area">
-        <div class="app-dashboard-close-sidebar">
-          <!-- Close button -->
-          <button id="close-sidebar" data-app-dashboard-toggle-shrink class="app-dashboard-sidebar-close-button show-for-medium" aria-label="Close menu" type="button">
-            <span aria-hidden="true"><a href="#"><i class="large fa fa-angle-double-left"></i></a></span>
-          </button>
-        </div>
-        <div class="app-dashboard-open-sidebar">
-          <button id="open-sidebar" data-app-dashboard-toggle-shrink class="app-dashboard-open-sidebar-button show-for-medium" aria-label="open menu" type="button">
-            <span aria-hidden="true"><a href="#"><i class="large fa fa-angle-double-right"></i></a></span>
-          </button>
-        </div>
-      </div>
-     <?php require('sidebar_menu.php'); ?>      
-    </div>
+			  <div class="app-dashboard-body off-canvas-wrapper">
+			    <div id="app-dashboard-sidebar" class="app-dashboard-sidebar position-left off-canvas off-canvas-absolute reveal-for-medium" data-off-canvas>
+			      <div class="app-dashboard-sidebar-title-area">
+			        <div class="app-dashboard-close-sidebar">
+			          <!-- Close button -->
+			          <button id="close-sidebar" data-app-dashboard-toggle-shrink class="app-dashboard-sidebar-close-button show-for-medium" aria-label="Close menu" type="button">
+			            <span aria-hidden="true"><a href="#"><i class="large fa fa-angle-double-left"></i></a></span>
+			          </button>
+			        </div>
+			        <div class="app-dashboard-open-sidebar">
+			          <button id="open-sidebar" data-app-dashboard-toggle-shrink class="app-dashboard-open-sidebar-button show-for-medium" aria-label="open menu" type="button">
+			            <span aria-hidden="true"><a href="#"><i class="large fa fa-angle-double-right"></i></a></span>
+			          </button>
+			        </div>
+			      </div>
+			     <?php require('sidebar_menu.php'); ?>      
+			    </div>
+			    
 			<!-- content-->
 			<div class="app-dashboard-body-content off-canvas-content" data-off-canvas-content>
 
@@ -114,66 +110,6 @@ require('header.php');
 			</div>
 		</div>
 	</div>
-
-<!--
-	<div class="container_12">
-		<h1>Editar perfil</h1>
-		<div class="grid_12" id="login">
-			<div class="grid_8 push_2" id="formulario_login">
-				<div class="grid_6 push_1" id="campos_login">
-					<?php 
-						if($this->session->flashdata('update_correcto'))
-						{
-					?>
-						<p><?=$this->session->flashdata('update_correcto')?></p>
-					<?php
-						}						
-					?>
-					<?php 
-						if($this->session->flashdata('update_incorrecto'))
-						{
-					?>
-						<p><?=$this->session->flashdata('update_incorrecto')?></p>
-					<?php
-						}						
-					?>
-					<form action="<?php echo base_url().'Login/update_user'; ?>" method="post" accept-charset="utf-8">
-						<input type="hidden" name="id" value="<?php echo $usuario[0]->id; ?>">
-						<label for="nombre">Nombre:</label>
-						<?php echo form_error('nombre'); ?>
-						<input type="text" name="nombre" id="nombre" value="<?php echo ($usuario[0]->nombre !=='') ? $usuario[0]->nombre:set_value('nombre'); ?>" placeholder="nombre"  />
-						</br></br>
-						<label for="nombre">Apellido paterno:</label>
-						<?php echo form_error('apaterno'); ?>
-						<input type="text" name="apaterno" id="apaterno" value="<?php echo ($usuario[0]->apaterno !=='') ? $usuario[0]->apaterno:set_value('apaterno'); ?>" placeholder="apellido paterno"  />
-						</br></br>
-						<label for="nombre">Apellido materno:</label>
-						<?php echo form_error('amaterno'); ?>
-						<input type="text" name="amaterno" id="amaterno" value="<?php echo ($usuario[0]->amaterno !=='') ? $usuario[0]->amaterno:set_value('amaterno'); ?>" placeholder="apellido paterno"  />
-						</br></br>
-						<label for="email">Email:</label>
-						<?php echo form_error('email'); ?>
-						<input type="text" name="email" id="email" value="<?php echo ($usuario[0]->email !=='') ? $usuario[0]->email:set_value('email'); ?>" placeholder="email"  />
-						</br></br>
-						<label for="telefono">Teléfono:</label>
-						<?php echo form_error('telefono'); ?>
-						<input type="text" name="telefono" id="telefono" value="<?php echo ($usuario[0]->telefono !=='') ? $usuario[0]->telefono:set_value('telefono'); ?>" placeholder="teléfono"  />
-						</br></br>
-						<input type="hidden" name="perfil" id='perfil' value="2">	
-						<input type="submit" name="submit" value="Editar" title="Editar"  />
-						<a href="<?php echo base_url(); ?>">Volver</a>
-					</form>	
-					<?php					
-						echo validation_errors();
-					?>					
-				</div>
-
-			</div>
-
-		</div>
-	</div>
-
--->
 
 <?php
 require('footer.php');
